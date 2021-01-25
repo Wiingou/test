2d4e976232122b39d9b537409b0da5767ec2d582
wiingou-admin <klebinc@wiingouassurance.com>
2021-01-25 14:46:10 +0100

    Next URL

--- src/main/java/com/example/test/TestController.java
+++ src/main/java/com/example/test/TestController.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class TestController {
     @GetMapping("/home")
     public String test() {
         return "WELCOME WORLD";
+    }
+
+    @GetMapping("/home/{text}")
+    public String test(@PathVariable String text) {
+        return "WELCOME "+ text;
     }
 }
